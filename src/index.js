05c7712da8d1c71d21dfc7b2717c9c15a50f3cc9
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2022-12-29 09:44:17 -0600

    moved the index.js folder out of scripts into src

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// this file is in the src because react won't start unless we have it in a src folder in website1
+// from what I've seen we could eject the webpack and make changes so it will start in the public/script folder
+// but I'm not completely sure what I'm doing and want to check in with the rest of you before I do anything like that
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const aElement = <p> this is being rendered to the website using react</p>
+
+root.render(aElement)
+
 // a lot of the commented out code is from me, Henry, and probably doesn't work, I'm just keeping it around just in case
 
 // import './loginBox.css';
@@ -15,11 +25,6 @@ import ReactDOM from 'react-dom/client';
 //         </form>
 //     </div>
 // )
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-const aElement = <p> this is being rendered to the website using react</p>
-
-root.render(aElement)
 
 // function logPopup() {
 //     root.render(logPopup);
