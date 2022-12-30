c823c36bf9cfc555878264ec3b95bbd8650486ec
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2022-12-30 09:58:01 -0600

    edited loginscript to include more of the new popups

--- public/Script/LoginScript.js
+++ public/Script/LoginScript.js
@@ -7,3 +7,18 @@ document.querySelector(".popup .close-btn").addEventListener("click",function(){
 document.querySelector(".popup .form .form-element input[type='submit']").addEventListener("click",function(){
     document.querySelector(".popup").classList.remove("active");
 });
+document.querySelector("#show-SignUp").addEventListener("click",function(){
+    document.querySelector(".UpPopup").classList.add("active");
+    document.querySelector(".LogPopup").classList.remove("active");
+});
+document.querySelector(".LogPopup .form .form-element input[type='submit']").addEventListener("click",function(){
+    document.querySelector(".LogPopup").classList.remove("active");
+});
+document.querySelector("#show-Login").addEventListener("click",function(){
+    document.querySelector(".UpPopup").classList.remove("active");
+    document.querySelector(".LogPopup").classList.add("active");
+});
+document.querySelector(".UpPopup .form .form-element input[type='submit']").addEventListener("click",function(){
+    document.querySelector(".LogPopup").classList.remove("active");
+    document.querySelector(".UpPopup").classList.remove("active");
+});
