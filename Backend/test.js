775fa9af6ae6b9a0237a0938e518db4c2f891e42
Redmite <59855327+Redmite@users.noreply.github.com>
2022-12-30 23:44:01 -0600

    Created backend folder and some basic code for tests.

---
+++ Backend/test.js
@@ -0,0 +1,3 @@
+var db = require('./db_broker.js'); //import the db_broker.js file so we can use the newAccount function
+
+db.newAccount('mrfooder07', 'Redmite', 'dummyPassword', 'Alsaedi', 'Yusuf'); //call the newAccount function and pass it the data we want to insert into the database
