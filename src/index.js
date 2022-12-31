1f2bfea98c34e6888b38ba051f818171ef9a7465
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2022-12-31 11:11:13 -0600

    deleted useless comments and rendered HomePage elements using react

--- src/index.js
+++ src/index.js
@@ -1,35 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOMClient from 'react-dom/client';
+import HomePage from "./HomePage";
 
 // this file is in the src because react won't start unless we have it in a src folder in website1
 // from what I've seen we could eject the webpack and make changes so it will start in the public/script folder
 // but I'm not completely sure what I'm doing and want to check in with the rest of you before I do anything like that
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const aElement = <p> this is being rendered to the website using react</p>
+// const aElement = HomePage
 
-root.render(aElement)
+// root.render(aElement)
 
-// a lot of the commented out code is from me, Henry, and probably doesn't work, I'm just keeping it around just in case
-
-// import './loginBox.css';
-// const loginPopup = (
-//     <div className="center">
-//         <p>test</p>
-//         <form>
-//             <label for="username">Username:</label>
-//             <label for="password">Password:</label>
-
-//             <input type="text" id="username" name="username"></input>
-//         </form>
-//     </div>
-// )
-
-// function logPopup() {
-//     root.render(logPopup);
-// }
-
-
-
-
+ReactDOMClient.createRoot(document.getElementById("root")).render(<HomePage />);
