3595e1e4d7af57f170ad9bb44ef9790e3c00ba47
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2022-12-31 09:59:51 -0600

    finished editing loginscript to include new page

--- public/Script/LoginScript.js
+++ public/Script/LoginScript.js
@@ -1,3 +1,4 @@
+// this is for the home page pop up, and is temporary
 document.querySelector("#show-login").addEventListener("click",function(){
     document.querySelector(".popup").classList.add("active");
 });
@@ -7,7 +8,12 @@ document.querySelector(".popup .close-btn").addEventListener("click",function(){
 document.querySelector(".popup .form .form-element input[type='submit']").addEventListener("click",function(){
     document.querySelector(".popup").classList.remove("active");
 });
-document.querySelector("#show-SignUp").addEventListener("click",function(){
+// this is for the home page, to show the login/sign-up page
+document.querySelector("#join-button").addEventListener("click",function(){
+    document.querySelector(".UpPopup").classList.add("active");
+});
+// this is for the new signUp page and is in development
+document.querySelector("#join-button").addEventListener("click",function(){
     document.querySelector(".UpPopup").classList.add("active");
     document.querySelector(".LogPopup").classList.remove("active");
 });
@@ -19,6 +25,5 @@ document.querySelector("#show-Login").addEventListener("click",function(){
     document.querySelector(".LogPopup").classList.add("active");
 });
 document.querySelector(".UpPopup .form .form-element input[type='submit']").addEventListener("click",function(){
-    document.querySelector(".LogPopup").classList.remove("active");
     document.querySelector(".UpPopup").classList.remove("active");
 });
