a74634c83ac0747146cb5acfaf20338624ea6751
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2022-12-31 18:17:51 -0600

    removed unneeded comments and imports

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react';
 import ReactDOMClient from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import HomePage from "./Pages/HomePage";
+
 import App from "./App";
 
 // this file is in the src because react won't start unless we have it in a src folder in website1
@@ -17,27 +12,5 @@ import App from "./App";
 // const aElement = HomePage
 
 // root.render(aElement)
-
-// 
-// const router = createBrowserRouter ([
-//   {
-//     path: "/",
-//     element: <HomePage />
-//   }
-// ]);
-
-
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
-
-// ReactDOMClient.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider Router={router} />
-//   </React.StrictMode>
-// );
 
 ReactDOMClient.createRoot(document.getElementById("root")).render(<App />);
