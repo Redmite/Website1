49d17a857d3755fc335913dcdf3df238222d533d
Dreadfall <116603758+Dreadfall@users.noreply.github.com>
2023-01-01 02:00:45 -0600

    Created MentorInfoPage.jsx (under development)

---
+++ src/Pages/MentorInfoPage.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+
+// this is still heavily in development 
+
+function MentorFormPage() {
+  <div class="mentorWrapper">
+    <div class="container">
+
+      <div class="basicInfoForm">
+        <h1>Basic Info</h1>
+        <div class="form-element">
+          <label for="Profile Name">Profile</label>
+          <input type="text" id="Profession" placeholder="Enter Your profile name"/>
+        </div>
+        <div class="form-element">
+          <label for="Profession">Profession:</label>
+          <input type="text" id="Profession" placeholder="Enter Your Profession"/>
+        </div>
+        <div class="form-element">
+          <label for="Professional email">Professional email:</label>
+          <input type="text" id="Profession email" placeholder="Enter your professional email"/>
+        </div>
+        <div class="form-element">
+          <label for="Languages">Profession:</label>
+          <input type="text" id="Languages" placeholder="Enter what languages do you speak"/>
+        </div>
+        <div class="form-element">
+          <label for="Basic Description">Description:</label>
+          <input type="text" id="Basic Description" placeholder="Give a description of yourself"/>
+        </div>
+      </div>
+
+      <div class="experienceForm">
+        <h1>Basic Info</h1>
+        <div class="form-element">
+          <label for="Ocupation">Current Ocupation:</label>
+          <input type="text" id="Ocupation" placeholder="Enter your Current Ocupation"/>
+        </div>
+        <div class="form-element">
+          <label for="PreviousOcupations">Past Experience</label>
+          <input type="text" id="PreviousOcupations" placeholder="Enter your Previous Experience "/>
+        </div>
+        <div class="form-element">
+          <label for="EducationLevel">How smart are you</label>
+          <input type="text" id="EducationLevel" placeholder="high school/college diplomas"/>
+        </div>
+        <div class="form-element">
+          <label for="Languages">Profession:</label>
+          <input type="text" id="Languages" placeholder="Enter what languages do you speak"/>
+        </div>
+        <div class="form-element">
+          <label for="Basic Description">Description:</label>
+          <input type="text" id="Basic Description" placeholder="Give a description of yourself"/>
+        </div>
+      </div>
+    </div>
+  </div>
+}
