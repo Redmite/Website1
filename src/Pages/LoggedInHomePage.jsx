dbb9dae5d3ff105d644ce0ff257c13dc013e62ad
Borseman1 <24768792+Borseman1@users.noreply.github.com>
2023-01-03 00:03:45 -0600

    Forgot to commit last time

--- src/Pages/LoggedInHomePage.jsx
+++ src/Pages/LoggedInHomePage.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 // CSS imports
 import "../Css/main.css";
@@ -6,51 +5,70 @@ import "../Css/loggedIn/topBar.css";
 import "../Css/loggedIn/mainContentLoggedIn.css";
 
 // home page function, this is what we actually see
-function  LoggedInHomePage() {
-    return (
-    
+function LoggedInHomePage() {
+  return (
     <div class="mainWrapper">
-            <div class="topBar-LoggedIn">
-                    <div class="logo"><img src="logo.png" class="logoImg" alt="missing logo" /></div>
-                    <form class="topSearch" action="#news">
-                    <input type="text" placeholder="What skill do you want to learn?" class="searchBar"/>
-                    </form>
-                    <div class="topBar-LoggedInRightSide">
-                        <li class="topBarCoach"><a>Creator</a></li>
-                        <li class="topBarOrders"><a>Orders</a></li>
-                        <li class="topBarSchedule"><a>Schedule</a></li>
-                        <li class="topBarInbox"><img src="inbox.svg" class="topBarInboximg"></img></li>
-                        <li class="topBarAccountProfile"><a href="#news"><img src="placeHolder.jpg" class="topBarAccountProfileImg"></img></a></li>
-                    </div></div>
-            <div class="mainContentLoggedIn">
-                <div> Reccomended Categories</div>
-                <div class="reccomendedCategories">
-                        <li class="reccomendedCategoriesPanel"></li>
-                        <li class="reccomendedCategoriesPanel"></li>
-                        <li class="reccomendedCategoriesPanel"></li>
-                        <li class="reccomendedCategoriesPanel"></li>
-                        <li class="reccomendedCategoriesPanel"></li>
-                    </div>
-                    <div> Reccomended Coaching Sessions</div>
-                    <div class="reccomendedCoachSession">
-                        <li class="reccomendedCoachSessionPanel"></li>
-                        <li class="reccomendedCoachSessionPanel"></li>
-                        <li class="reccomendedCoachSessionPanel"></li>
-                        <li class="reccomendedCoachSessionPanel"></li>
-                        <li class="reccomendedCoachSessionPanel"></li>
-                    </div> 
-                    <div> Reccomended Creators</div>
-                    <div class="reccomendedCreators">
-                        <li class="reccomendedCreatorsPanel"></li>
-                        <li class="reccomendedCreatorsPanel"></li>
-                        <li class="reccomendedCreatorsPanel"></li>
-                        <li class="reccomendedCreatorsPanel"></li>
-                        <li class="reccomendedCreatorsPanel"></li>
-                    </div> 
-                </div>
-                <div class="bottomBar"></div>
-        <script></script></div>
-    ); 
+      <div class="topBar-LoggedIn">
+        <div class="logo">
+          <img src="logo.svg" class="logoImg" alt="missing logo" />
+        </div>
+        <form class="topSearch" action="#news">
+          <input
+            type="text"
+            placeholder="What skill do you want to learn?"
+            class="searchBar"
+          />
+        </form>
+        <div class="topBar-LoggedInRightSide">
+          <li class="topBarCoach">
+            <a>Creator</a>
+          </li>
+          <li class="topBarOrders">
+            <a>Orders</a>
+          </li>
+          <li class="topBarSchedule">
+            <a>Schedule</a>
+          </li>
+          <li class="topBarInbox">
+            <img src="inbox.svg" class="topBarInboximg"></img>
+          </li>
+          <li class="topBarAccountProfile">
+            <a href="#news">
+              <img src="placeHolder.jpg" class="topBarAccountProfileImg"></img>
+            </a>
+          </li>
+        </div>
+      </div>
+      <div class="mainContentLoggedIn">
+        <div> Reccomended Categories</div>
+        <div class="reccomendedCategories">
+          <li class="reccomendedCategoriesPanel"></li>
+          <li class="reccomendedCategoriesPanel"></li>
+          <li class="reccomendedCategoriesPanel"></li>
+          <li class="reccomendedCategoriesPanel"></li>
+          <li class="reccomendedCategoriesPanel"></li>
+        </div>
+        <div> Reccomended Coaching Sessions</div>
+        <div class="reccomendedCoachSession">
+          <li class="reccomendedCoachSessionPanel"></li>
+          <li class="reccomendedCoachSessionPanel"></li>
+          <li class="reccomendedCoachSessionPanel"></li>
+          <li class="reccomendedCoachSessionPanel"></li>
+          <li class="reccomendedCoachSessionPanel"></li>
+        </div>
+        <div> Reccomended Creators</div>
+        <div class="reccomendedCreators">
+          <li class="reccomendedCreatorsPanel"></li>
+          <li class="reccomendedCreatorsPanel"></li>
+          <li class="reccomendedCreatorsPanel"></li>
+          <li class="reccomendedCreatorsPanel"></li>
+          <li class="reccomendedCreatorsPanel"></li>
+        </div>
+      </div>
+      <div class="bottomBar"></div>
+      <script></script>
+    </div>
+  );
 }
 
 export default LoggedInHomePage;
