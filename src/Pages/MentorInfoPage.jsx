a1f61cad82088b287e08d732496e341546beff23
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2023-01-03 11:14:26 -0600

    consolidated forms into one and commented out new functions

--- src/Pages/MentorInfoPage.jsx
+++ src/Pages/MentorInfoPage.jsx
@@ -15,47 +15,59 @@ function Textf(props) {
   </div>
   );
 }
-function SubButton(props) {
-  return (           
-  <div class="form-element">
-    <button onClick={props.from}>Back</button>
-    <button onClick={props.to}>Next</button>    
-  </div>
-  );
-}
+
+// these functions are a work in progress
+
+// function SwitchPages(props) {
+//   var nextForm = props.to
+//   document.getElementsByClassName("basicInfoForm experienceForm payForm").style.opacity = "1";
+//   document.getElementById({nextForm}).style.opacity = "1";
+// }
+// function SubButton(props) {
+//   return (           
+//   <div class="form-element">
+//     <button onClick={() => SwitchPages(to = props.back)}>Back</button>
+//     <button onClick={() => SwitchPages(to = props.next)}>Next</button>    
+//   </div>
+//   );
+// }
+
 
 function MentorInfoPage() {
   return (
   <>
     <div class="mentorPageWrapper">
       <div class="container">
-        <div class="basicInfoForm">
-          <h2>Basic Info</h2>
-          <Textf info="Profile Name" id="Profile Name" enter="" />
-          <Textf info="Profession" id="Profession" enter="" />
-          <Textf info="Professional Email" id="Professional Email" enter="" />
-          <Textf info="Your Website Url" id="Website" enter="" />
-          <Textf info="Social Medias" id="Social Medias" enter="" />
-          <Textf info="Languages" id="Languages" enter="" />
-          <Textf info="Description" id="Description" enter="" />
-        </div>
+        <div class="testContainer">
+          <div class="basicInfoForm" id="basic">
+            <h2>Basic Info</h2>
+            <Textf info="Profile Name" id="Profile Name" enter="" />
+            <Textf info="Profession" id="Profession" enter="" />
+            <Textf info="Professional Email" id="Professional Email" enter="" />
+            <Textf info="Your Website Url" id="Website" enter="" />
+            <Textf info="Social Medias" id="Social Medias" enter="" />
+            <Textf info="Languages" id="Languages" enter="" />
+            <Textf info="Description" id="Description" enter="" />
+          </div>
 
-        <div class="experienceForm">
-          <h2>Experience</h2>
-          <Textf info="Field of Work" id="Field" enter="" />
-          <Textf info="Current Ocupation" id="Ocupation" enter="" />
-          <Textf info="Past Experience" id="Experience" enter="" />
-          <Textf info="How Smart Are you" id="Education Level" enter="" />
-          <Textf info="Diplomas or Certifications" id="Diplomas" enter="" />
-        </div>
+          <div class="experienceForm">
+            <h2>Experience</h2>
+            <Textf info="Field of Work" id="Field" enter="" />
+            <Textf info="Current Ocupation" id="Ocupation" enter="" />
+            <Textf info="Past Experience" id="Experience" enter="" />
+            <Textf info="How Smart Are you" id="Education Level" enter="" />
+            <Textf info="Diplomas or Certifications" id="Diplomas" enter="" />
+          </div>
 
-        <div class="payForm">
-          <h2>Pay and Time</h2>
-          <Textf info="Your Hourly Rate" id="HourlyRate" enter="" />
-          <Textf info="credit info" id="Field" enter="" />
-          <Textf info="rest of this is placeholder for payment method" id="Field" enter="" />
-          <Textf info="Field of Work" id="Field" enter="" />
-          <Textf info="Field of Work" id="Field" enter="" />
+          <div class="payForm">
+            <h2>Pay and Time</h2>
+            <Textf info="Your Hourly Rate" id="HourlyRate" enter="" />
+            <Textf info="credit info" id="Field" enter="" />
+            <Textf info="rest of this is placeholder for payment method" id="Field" enter="" />
+            <Textf info="Field of Work" id="Field" enter="" />
+            <Textf info="Field of Work" id="Field" enter="" />
+            <input type="submit" value="Continue?"/>
+          </div>
         </div>
       </div>
     </div>
