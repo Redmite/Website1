62749dc0692b17d6fb1b89d4676f8a73c4132571
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2023-01-05 05:32:05 -0600

    changed calendar to take text boxs instead of buttons

--- src/Script/Time.js
+++ src/Script/Time.js
@@ -1,36 +1,54 @@
 import React from 'react'
 import {useState} from 'react';
 
-const time = ['08:00','09:00','10:00','14:00','15:00'] // this is the array of times that will be displayed
+var sessionTime = "no Time Selected";
 
 function Times(props) {
 
- const [event, setEvent] = useState(null) // this is the use state hook (look at react docs for more info)
- const [info, setInfo] = useState(false)
+ const [info, setInfo] = useState(false); // this is the use state hook (look at react docs for more info)
+ const [startTime, setStartTime] = useState('');
+ const [endTime, setEndTime] = useState('');
 
 // this is the event that is passed in when the button is clicked. it is the button itself. it is passed in as a parameter in the displayInfo function
- function displayInfo(e) {
-   setInfo(true); // this sets the info (see line 29) to visible
-   setEvent(e.target.innerText); // this 
+ function submitInfo() {
+  setInfo(true); // this sets the info (see line 30) to visible
+  sessionTime = startTime + " to " + endTime + " " + props.date.toDateString(); //this gets sent to back end for processing and storage
 }
+// make it so the function below creates two text boxs where the user can enter the time they want to start and end their session.
+const handleStart = event => {
+  setStartTime(event.target.value);
+  setInfo(false)
+  console.log('value is:', event.target.value);
+};
+const handleEnd = event => {
+  setEndTime(event.target.value);
+  setInfo(false)
+  console.log('value is:', event.target.value);
+};
 
 return (
  
  <div className="times">
-   {time.map(times => { // this is a map function that will loop through the array of times and display them
-    return (
     <div>
       <label for="startTime">When would you like to start your sessions?</label>
-      <input type="text" placeholder='please enter only times that end with :30 or :00' />
-      <button onClick={(e)=> displayInfo(e)}>  {times} </button> 
-    </div>// when the button is clicked, it will call the display function and pass itself. it will then display the time for that button
-        )
-     })} // what is the ${event} int he statement below
+      <input type="text" placeholder='30 minute increments please' onChange={handleStart} value={startTime}/>
+      <label for="startTime">When would you like to end your sessions?</label>
+      <input type="text" placeholder='30 minute increments please' onChange={handleEnd} value={endTime} />
+      <button onClick={()=> submitInfo()}>Enter Session </button> 
+    </div>
+
+
     <div>
-      {info ? `Your session is set to ${event} ${props.date.toDateString()}` : null} 
+      {info ? `Your session is set from ${startTime} to ${endTime} ${props.date.toDateString()}` : null} 
     </div> 
  </div> // this is a ternary operator. if info is true, it will display the time and date. if not, it will display nothing
-  )
+  ) // why is there a $ in front of the event and props.date.toDateString()? this is a template literal. it allows you to use variables in a string. look at the link below for more info
 }
  
 export default Times;
+
+
+// {time.map(times => { // this is a map function that will loop through the array of times and display them
+// return (
+  //  {times => {
+  //   return (    )}}
