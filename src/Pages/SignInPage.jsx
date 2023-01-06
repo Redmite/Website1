70a9ec93b7680963b71879542384da0c4b4de210
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2023-01-06 05:50:00 -0600

    small changes, fixed remember me box error

--- src/Pages/SignInPage.jsx
+++ src/Pages/SignInPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState } from "react";
 
-var info = []
+var info = [];
 
 function SignInPage() {
 
@@ -9,18 +9,15 @@ function SignInPage() {
   const [password, setPassword] = useState();
   const [remember, setRemember] = useState(false);
 
-
   const handleEmail = event => {
     setEmail(event.target.value);
-    console.log('value is:', event.target.value);
   };
   const handlePassword = event => {
     setPassword(event.target.value);
-    console.log('value is:', event.target.value);
   };
-  const handleRemember = event => {
-    setRemember(event.target.value);
-    console.log('value is:', event.target.value);
+  const handleRemember = (e) => {
+    const checked = e.target.checked;
+    checked ? setRemember(true) : setRemember(false);
   };
   const submitInfo = () => {
     info = [email, password, remember]
@@ -44,11 +41,11 @@ function SignInPage() {
               <input type="password" id="password" placeholder="Enter Password" onChange={handlePassword} value={password}/>
              </div>
             <div class="form-element">
-              <input type="checkbox" id="remember-me" onChange={handleRemember} value={remember}/>
+              <input type="checkbox" id="remember-me" onChange={handleRemember} value={remember}  />
               <label for="remember-me">Remember me</label>
             </div>
             <div class="form-element">
-              <input type="submit" value="Continue" onChange={submitInfo}/>
+              <input type="submit" value="Continue" onClick={submitInfo}/>
             </div>
             <div class="form-element">
               <a href="/placeholder">Forgot password?</a>
