5ca95d7cad449ae304a62e2ef7ca7b544b29c838
Dreadfall <116603758+Dreadfall@users.noreply.github.com>
2023-01-06 11:11:48 -0600

    listingCreationPage.jsx can now submit to backend (mainly added various functions and variables from line 14-61

--- src/Pages/ListingCreationPage.jsx
+++ src/Pages/ListingCreationPage.jsx
@@ -7,9 +7,58 @@ import Time from '../Script/Time.js';
 import "../Css/loggedIn/listingCreation.css";
 
 function ListingCreationPage() {
+  // variables for calanders
   const [date, setDate] = useState(new Date());
   const [showTime, setShowTime] = useState(false) 
 
+  // variables for inputs
+  const [profilePic, setProfilePic] = useState();
+  const [experience, setExperience] = useState();
+  const [certifications, setCertifications] = useState();
+  const [funFact, setFunFact] = useState();
+  const [Prequisetes, setPrequisetes] = useState();
+  const [learnMaterial, setLearnMaterial] = useState();
+  const [choose, setChoose] = useState();
+  const [projectPic, setProjectPic] = useState();
+    // when backend gets the data, this will be set up to where inside the use state parenthesis,
+    // will be the data from the data base
+
+  // list for submitting
+  var info = [];
+
+  // functions for inputs
+  const handleProfilePic = event => {
+    setProfilePic(event.target.value);
+  };
+  const handleExperience = event => {
+    setExperience(event.target.value);
+  };
+  const handleCertifications = event => {
+    setCertifications(event.target.value);
+  };
+  const handleFunFact = event => {
+    setFunFact(event.target.value);
+  };
+  const handlePrequisetes = event => {
+    setPrequisetes(event.target.value);
+  };
+  const handleLearnMaterial = event => {
+    setLearnMaterial(event.target.value);
+  };
+  const handleChoose = event => {
+    setChoose(event.target.value);
+  };
+  const handleProjectPic = event => {
+    setProjectPic(event.target.value);
+  };
+  // this function will submit all the data to the database
+  const submitInfo = () => {
+    info = [profilePic, experience, certifications, funFact, Prequisetes, learnMaterial, choose, projectPic]
+    // some line to submit it to the database
+    // maybe a return boolean
+    // if true, then redirect to the logged in page
+    // if false then display an error message
+  };
   return (
     <div class="mainWrapper">
       <div class="container">
@@ -17,23 +66,23 @@ function ListingCreationPage() {
           <div class="profilePic">
             <div class="profilePicInfo">
               <label for="profilePic">Insert a Picture of yourself here!</label>
-              <input type="file" id="ProfilePic" name="Profile Picture"/>
+              <input type="file" id="ProfilePic" name="Profile Picture" onChange={handleProfilePic} value={profilePic}/>
             </div>
           </div>
           <div class="experienceQualifications">
             <h3>Name</h3>
-            <input type="text" placeholder="What experience do you have relating to this listing"/>
-            <input type="text" placeholder="What certifications or diplomas do you have relating to this listing"/>
-            <input type="text" placeholder="Give a fun fact about yourself or the job listing"/>
-            <input type="text" placeholder="Knowledge or material prerequisetes before your session "/>
+            <input type="text" placeholder="What experience do you have relating to this listing" onChange={handleExperience} value={experience}/>
+            <input type="text" placeholder="What certifications or diplomas do you have relating to this listing" onChange={handleCertifications} value={certifications}/>
+            <input type="text" placeholder="Give a fun fact about yourself or the job listing" onChange={handleFunFact} value={funFact}/>
+            <input type="text" placeholder="Knowledge or material prerequisetes before your session " onChange={handlePrequisetes} value={Prequisetes}/>
           </div>
           <div class="details">
-            <input type="text" placeholder="What do you offer and what they will learn"/>
-            <input type="text" placeholder="Why should they choose you over others"/>
+            <input type="text" placeholder="What do you offer and what they will learn" onChange={handleLearnMaterial} value={learnMaterial}/>
+            <input type="text" placeholder="Why should they choose you over others" onChange={handleChoose} value={choose}/>
           </div>
           <div class="projectPic">
           <p>Project they've worked on</p>
-          <input type="file" id="ProjectPic" name="Project Picture"/>
+          <input type="file" id="ProjectPic" name="Project Picture" onChange={handleProjectPic} value={projectPic}/>
           </div>
           <h1 className='header'>Dates Avalible</h1>
           <div>
@@ -55,6 +104,9 @@ function ListingCreationPage() {
           )
           }
           <Time showTime={showTime} date={date}/>
+
+          <button onClick={submitInfo}>Save</button>
+          <button onClick="/placeholderForListingPage">See your listing</button>
         </div>
       </div>       
     </div>
