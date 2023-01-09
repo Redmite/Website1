1df93a2aa27a2656fc15b824904eadaf5eb1a18b
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2023-01-08 19:25:02 -0600

    moved categories for logged out page to new script

--- src/Pages/LoggedInHomePage.jsx
+++ src/Pages/LoggedInHomePage.jsx
@@ -1,72 +1,9 @@
 import React from "react";
+import CreatorCategories from "../Script/CreatorCategories";
 // CSS imports
 import "../Css/main.css";
 import "../Css/loggedIn/topBar.css";
 import "../Css/loggedIn/mainContentLoggedIn.css";
-
-function CreatorCategories(props) {
-  { if (props.category === "null") {
-    return (
-      <div class="mainContentLoggedIn">
-        <div> Reccomended Categories</div>
-        <div class="reccomendedCategories">
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-        </div>
-        <div> Reccomended Coaching Sessions</div>
-        <div class="reccomendedCoachSession">
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-        </div>
-        <div> Reccomended Creators</div>
-        <div class="reccomendedCreators">
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-        </div>
-      </div>
-    );
-  } else {
-    return (
-      <div class="mainContentLoggedIn">
-    
-        <div> Reccomended {props.category}</div>
-        <div class="reccomendedCategories">
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-        </div>
-        <div> Reccomended Coaching Sessions</div>
-        <div class="reccomendedCoachSession">
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-        </div>
-        <div> Reccomended Creators</div>
-        <div class="reccomendedCreators">
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-        </div>
-      </div>
-    );
-  }};
-}
-
 
 function LoggedInHomePage(props) {
   return (
