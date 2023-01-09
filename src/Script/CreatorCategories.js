d1ed47d71111ee8b5dc83cc23dd7ab0fbf453275
DreadFall <116603758+Dreadfall@users.noreply.github.com>
2023-01-08 20:06:16 -0600

    variables for specific categories now, ready for back-end

--- src/Script/CreatorCategories.js
+++ src/Script/CreatorCategories.js
@@ -4,41 +4,28 @@ import "../Css/main.css";
 import "../Css/loggedIn/topBar.css";
 import "../Css/loggedIn/mainContentLoggedIn.css";
 
+
 function CreatorCategories(props) {
-  { if (props.category === "null") {
-    return (
-      <div class="mainContentLoggedIn">
-        <div> Reccomended Categories</div>
-        <div class="reccomendedCategories">
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-          <li class="reccomendedCategoriesPanel"></li>
-        </div>
-        <div> Reccomended Coaching Sessions</div>
-        <div class="reccomendedCoachSession">
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-          <li class="reccomendedCoachSessionPanel"></li>
-        </div>
-        <div> Reccomended Creators</div>
-        <div class="reccomendedCreators">
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-          <li class="reccomendedCreatorsPanel"></li>
-        </div>
-      </div>
-    );
-  } else {
+
+  if (props.category === "null") { 
+    var category = "General";
+  } else if (props.category === "commonCore") {
+      var category = "Common Core";
+  } else if (props.category === "websiteDesign") {
+      var category = "Website Design";
+  } else if (props.category === "Language") {
+      var category = "Common Core";
+  } else if (props.category === "onlineBusiness") {
+      var category = "Online Business";
+  } else if (props.category === "designing") {
+      var category = "Designing";
+  }
+    // here we will pass back the catagory to database, and it will pass back all the 
+    // sessions that are in that catagory
     return (
       <div class="mainContentLoggedIn">
     
-        <div> Reccomended {props.category}</div>
+        <div> Reccomended {category} Sessions </div>
         <div class="reccomendedCategories">
           <li class="reccomendedCategoriesPanel"></li>
           <li class="reccomendedCategoriesPanel"></li>
@@ -64,7 +51,6 @@ function CreatorCategories(props) {
         </div>
       </div>
     );
-  }};
 }
 
 export default CreatorCategories;
