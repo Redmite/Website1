be8a33dbfb5c6a57f3d7b7f557b41bbeda4093f9
Borseman1 <24768792+Borseman1@users.noreply.github.com>
2023-01-29 18:43:44 -0600

    Fixed a shiton of bugs i created lol

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -16,7 +16,6 @@ import onlineBusiness from "../images/onlineBusiness.jpeg";
 import language from "../images/language.jpeg";
 import websiteDesign from "../images/websiteDesign.jpeg";
 import illustrations from "../images/Illustrations.jpeg";
-import searchBarImg from "../images/SearchBar.svg";
 
 // component imports
 import SearchBar from "../Script/SearchBar";
@@ -38,7 +37,6 @@ function HomePage() {
                 placeholder="What skill do you want to learn?"
                 class="searchBar"
               />
-              <button src={searchBarImg}></button>
             </form>
             <div class="topOptionsRightSide">
               <li class="displayMd">
